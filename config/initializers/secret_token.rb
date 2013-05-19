53727bd9f48ce065dd2cb8b8e940ff8d04e77105
acacheung <acacheung@gmail.com>
2013-05-19 14:48:03 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scorekeeper::Application.config.secret_token = '44cfd0454ef733b7503c4067285f0ffa38fbc467bee437e6ff2fcdd985a4cabefd8b2aed65397ca18647bb73e2dffbd987c48e04456c5c7ab7a441e09e7b1348'
